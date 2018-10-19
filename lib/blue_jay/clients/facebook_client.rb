b36d803980fc00a7cad8c55c41703237c09c8d82
Stephen Roos <stephen.m.roos@gmail.com>
2018-10-19 13:37:49 -0700

    Upgrade FacebookClient to 2.10, add appsecret_proof

--- lib/blue_jay/clients/facebook_client.rb
+++ lib/blue_jay/clients/facebook_client.rb
@@ -1,10 +1,12 @@
 
+require 'openssl'
+
 module BlueJay
   class FacebookClient < Client
 
     LONG_TOKEN_EXPIRES_IN = 5184000   # 60 days
 
-    filtered_attributes :client_id, :client_secret, :access_token
+    filtered_attributes :client_id, :client_secret, :access_token, :appsecret_proof
 
     # ============================================================================
     # Client Initializers and Public Methods
@@ -12,12 +14,12 @@ module BlueJay
 
     def initialize(options={})
       options[:site] ||= 'https://graph.facebook.com'
-      options[:path_prefix] ||= '/v2.9'
+      options[:path_prefix] ||= '/v2.10'
       super(options)
     end
 
     def authorize_url(redirect_uri, options={})
-      uri_with_query("https://www.facebook.com/v2.9/dialog/oauth", options.merge(
+      uri_with_query("https://www.facebook.com/v2.10/dialog/oauth", options.merge(
         client_id: client_id,
         redirect_uri: redirect_uri
       ))
@@ -91,9 +93,11 @@ module BlueJay
       get('/me', options)
     end
 
-    def share(options={})
-      post('/me/feed', options)
-    end
+    # DEPRECATED
+    # see: https://developers.facebook.com/docs/graph-api/changelog/breaking-changes#login-4-24
+    # def share(options={})
+    #   post('/me/feed', options)
+    # end
 
     # ============================================================================
     # Photo Methods
@@ -107,14 +111,16 @@ module BlueJay
       get('/me/albums', options)
     end
 
-    def upload_photo(image_or_url, options={})
-      album_id = options.delete(:album_id)
-      options = multipart?(item: image_or_url) ?
-        options.merge(source: image_or_url) :
-        options.merge(url: image_or_url)
+    # DEPRECATED
+    # see: https://developers.facebook.com/docs/graph-api/changelog/breaking-changes#login-4-24
+    # def upload_photo(image_or_url, options={})
+    #   album_id = options.delete(:album_id)
+    #   options = multipart?(item: image_or_url) ?
+    #     options.merge(source: image_or_url) :
+    #     options.merge(url: image_or_url)
 
-      post("/#{album_id}/photos", options)
-    end
+    #   post("/#{album_id}/photos", options)
+    # end
 
     # ============================================================================
     # Page Methods
@@ -144,13 +150,23 @@ module BlueJay
       end
     end
 
+    def appsecret_proof
+      @appsecret_proof ||= generate_appsecret_proof(access_token)
+    end
+
+    def generate_appsecret_proof(token)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), client_secret, token) if token
+    end
+
     def get(path, params={})
-      params[:access_token] ||= access_token
+      params[:access_token]    ||= access_token
+      params[:appsecret_proof] ||= generate_appsecret_proof(params[:access_token])
       super
     end
 
     def post(path, params={})
-      params[:access_token] ||= access_token
+      params[:access_token]    ||= access_token
+      params[:appsecret_proof] ||= generate_appsecret_proof(params[:access_token])
       super
     end
 
--- lib/blue_jay/version.rb
+++ lib/blue_jay/version.rb
@@ -1,3 +1,3 @@
 module BlueJay
-  VERSION = "4.2.10"
+  VERSION = "4.2.11"
 end
