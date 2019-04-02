a5004e0c0307b2a6173516622840bf09e50ab333
Casey <cvollmer@careerarc.com>
2019-04-02 09:40:45 -0700

    enterprise twitter client

--- lib/blue_jay/clients/twitter_enterprise_client.rb
+++ lib/blue_jay/clients/twitter_enterprise_client.rb
@@ -1,6 +1,6 @@
 
 module BlueJay
-  class TwitterEnterpriseClient < OAuth2Client
+  class TwitterEnterpriseClient < OAuthClient
 
     # ============================================================================
     # Client Initializers and Public Methods
@@ -8,18 +8,23 @@ module BlueJay
 
     def initialize(options={})
       options[:site] ||= 'https://data-api.twitter.com'
-      options[:authorize_url] ||= 'https://api.twitter.com/oauth/authorize'
-      options[:token_url]     ||= 'https://api.twitter.com/oauth/access_token'
       options[:path_prefix] ||= ''
 
       super(options)
     end
 
     def historical(start_date = 1.day.ago.to_date, end_date = 1.minute.ago.to_date, options={})
-      post('/insights/engagement/historical', options.merge(start: start_date.to_s, end: end_date.to_s))
+      post('/insights/engagement/historical', options.merge("start": start_date.to_s, "end": end_date.to_s, engagement_types: ["impressions", "engagements", "favorites", "retweets", "replies", "video_views", "url_clicks","hashtag_clicks", "detail_expands", "permalink_clicks", "email_tweet", "user_follows", "user_profile_clicks"], groupings: {by_tweet_by_day: {group_by: ["tweet.id", "engagement.day", "engagement.type"]}}).to_json)
     end
 
     protected
+
+    def add_standard_headers(headers={})
+      super(headers.merge(
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      ))
+    end
 
     def response_parser
       BlueJay::TwitterParser
