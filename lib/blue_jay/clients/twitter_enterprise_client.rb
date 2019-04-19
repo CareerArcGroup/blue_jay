b9edf01f5c989475c30c45ac27d61cfe0cd12f32
Casey <cvollmer@careerarc.com>
2019-04-19 11:01:38 -0700

    update to twitter ent

--- lib/blue_jay/clients/twitter_enterprise_client.rb
+++ lib/blue_jay/clients/twitter_enterprise_client.rb
@@ -13,8 +13,12 @@ module BlueJay
       super(options)
     end
 
+    def totals(options={})
+      post('/insights/engagement/totals', options.merge(engagement_types: ["impressions", "engagements", "favorites", "retweets"], groupings: {by_tweet_by_type: {group_by: ["tweet.id", "engagement.type"]}}).to_json)
+    end
+
     def historical(start_date = 1.day.ago.to_date, end_date = 1.minute.ago.to_date, options={})
-      post('/insights/engagement/historical', options.merge("start": start_date.to_s, "end": end_date.to_s, engagement_types: ["impressions", "engagements", "favorites", "retweets", "replies", "video_views", "url_clicks","hashtag_clicks", "detail_expands", "permalink_clicks", "email_tweet", "user_follows", "user_profile_clicks"], groupings: {by_tweet_by_day: {group_by: ["tweet.id", "engagement.day", "engagement.type"]}}).to_json)
+      post('/insights/engagement/historical', options.merge("start": start_date.to_s, "end": end_date.to_s, engagement_types: ["impressions", "engagements", "favorites", "retweets", "replies", "video_views", "url_clicks","hashtag_clicks", "detail_expands", "permalink_clicks", "email_tweet", "user_follows", "user_profile_clicks"], groupings: {by_tweet_by_day: {group_by: ["tweet.id", "engagement.type", "engagement.day", "engagement.hour"]}}).to_json)
     end
 
     protected
--- lib/blue_jay/version.rb
+++ lib/blue_jay/version.rb
@@ -1,3 +1,3 @@
 module BlueJay
-  VERSION = "4.5.2.1"
+  VERSION = "4.5.2.2"
 end
