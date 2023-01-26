68ab7c7501a1625df3f884718b61a1d4535f1630
Josh Ofrane <jofrane@careerarc.com>
2023-01-26 11:16:20 -0800

    pol-5240 fix put request content_type bug - path doesnt exist for stringio

--- lib/blue_jay/client.rb
+++ lib/blue_jay/client.rb
@@ -113,7 +113,7 @@ module BlueJay
             Net::HTTP::Put::Multipart.new(request_uri, to_multipart_params(request.body)) :
             Net::HTTP::Put.new(request_uri).tap do |req|
               if request.body.respond_to?(:to_io) || request.body.is_a?(UploadIO)
-                request.headers['Content-Type'] = BlueJay::MIME.mime_type_for(request.body.path)
+                request.headers['Content-Type'] = request.body.content_type
                 request.headers['Content-Length'] = request.body.size
 
                 req.body_stream = request.body
